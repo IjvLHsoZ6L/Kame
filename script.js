2d0f915533d71ab89c9c81f49f153cc538c43e11
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-02-26 02:51:24 +0900

    rewrite

--- script.js
+++ script.js
@@ -1,154 +1,179 @@
 (function() {
 
-  const size = 4;
+  const size  = 4;
   const scale = 100;
 
-  const red = '#ffcccc';
+  const gray  = '#cccccc';
+  const red   = '#ffcccc';
+  const blue  = '#ccccff';
   const green = '#ccffcc';
-  const blue = '#ccccff';
 
-  var array = [];
-  var config = true;
-  var count = 0;
-  var holding = false;
-  var cursor = { i: 0, j: 0 };
+  const blank   = 0;
+  const obverse = 1;
+  const reverse = 2;
 
-  (function() {
+  var config;
+  var count, holding, I, J;
+  var state, table, image;
+  var saved = [[0, 0, 0, 0], [2, 2, 2, 2], [2, 2, 2, 2], [0, 2, 2, 0]];
+
+  (function initialize() {
+    config = false;
+    count = 0;
+    holding = false;
+    state = [];
+    table = [];
+    image = [];
     var tbody = document.getElementById('tbody');
     for (var i = 0; i < size; i++) {
-      array[i] = [];
+      state[i] = [];
+      table[i] = [];
+      image[i] = [];
       var tr = document.createElement('tr');
-      tbody.appendChild(tr);
       for (var j = 0; j < size; j++) {
-        array[i][j] = {};
-        array[i][j].exist = false;
-        array[i][j].omote = false;
-        array[i][j].td = document.createElement('td');
-        array[i][j].td.onclick = onclick(i, j);
-        array[i][j].td.style.width = scale + 'px';
-        array[i][j].td.style.height = scale + 'px';
-        array[i][j].img = document.createElement('img');
-        array[i][j].img.style.width = (0.9 * scale) + 'px';
-        tr.appendChild(array[i][j].td);
-        array[i][j].td.appendChild(array[i][j].img);
+        state[i][j] = saved[i][j];
+        table[i][j] = document.createElement('td');
+        image[i][j] = document.createElement('img');
+        image[i][j].style.width = (0.9 * scale) + 'px';
+        table[i][j].appendChild(image[i][j]);
+        table[i][j].style.width = scale + 'px';
+        table[i][j].style.height = scale + 'px';
+        table[i][j].style.textAlign = 'center';
+        table[i][j].onclick = onclick(i, j);
+        tr.appendChild(table[i][j]);
       }
+      tbody.appendChild(tr);
     }
-    update();
-    document.getElementById('button').onclick = toggleConfig;
-  })();
 
-  function toggleConfig() {
-    if (config) {
+    update();
+
+    document.getElementById('start').onclick = function() {
       config = false;
-      document.getElementById('button').value = 'Config';
-    }
-    else {
-      config = true;
-      document.getElementById('button').value = 'Start';
       count = 0;
       holding = false;
-      update();
-    }
-  }
+      load();
+    };
 
-  function update() {
-    for (var i = 0; i < size; i++) {
-      for (var j = 0; j < size; j++) {
-        array[i][j].img.style.visibility = array[i][j].exist ? 'visible' : 'hidden';
-        if (array[i][j].exist)
-          array[i][j].img.src = array[i][j].omote ? 'omote.png' : 'ura.png';
-        if (config)
-          array[i][j].td.style.background = green;
-        else if (selected(i, j))
-          array[i][j].td.style.background = red;
-        else if (available(i, j))
-          array[i][j].td.style.background = blue;
-        else
-          array[i][j].td.style.background = green;
-      }
-    }
-    document.getElementById('count').value = count;
-  }
+    document.getElementById('config').onclick = function() {
+      config = true;
+      count = 0;
+      holding = false;
+      load();
+    };
+
+    document.getElementById('save').onclick = function() {
+      for (var i = 0; i < size; i++)
+        for (var j = 0; j < size; j++)
+          saved[i][j] = state[i][j];
+    };
+  })();
 
   function onclick(i, j) {
     return function() {
       if (config) {
-        if (array[i][j].exist && array[i][j].omote)
-          array[i][j].omote = false;
-        else if (array[i][j].exist)
-          array[i][j].exist = false;
-        else {
-          array[i][j].exist = true;
-          array[i][j].omote = true;
-        }
+        state[i][j] = (state[i][j] + 1) % 3;
         update();
       }
-      else if (selected(i, j))
-        release();
-      else if (available(i, j))
-        move(i, j);
-      else if (!holding && array[i][j].exist)
-        hold(i, j);
+      else {
+        if (selected(i, j))
+          release();
+        else if (can_move(i, j))
+          move(i, j);
+        else if (can_hold(i, j))
+          hold(i, j);
+      }
     };
   }
 
-  function selected(i, j) {
-    return holding && i === cursor.i && j === cursor.j;
+  function update() {
+    document.getElementById('count').value = count;
+    for (var i = 0; i < size; i++) {
+      for (var j = 0; j < size; j++) {
+        if (state[i][j] === blank)
+          image[i][j].style.visibility = 'hidden';
+        else {
+          image[i][j].style.visibility = 'visible';
+          image[i][j].src = state[i][j] === obverse ? 'obverse.png' : 'reverse.png';
+        }
+        if (config)
+          table[i][j].style.background = gray;
+        else if (selected(i, j))
+          table[i][j].style.background = red;
+        else if (can_move(i, j))
+          table[i][j].style.background = blue;
+        else
+          table[i][j].style.background = green;
+      }
+    }
   }
 
-  function available(i, j) {
+  function selected(i, j) {
+    return holding && i === I && j === J;
+  }
+
+  function can_move(i, j) {
     if (!holding)
       return false;
-    if (array[i][j].exist)
+    if (state[i][j] !== blank)
       return false;
-    if (Math.abs(i - cursor.i) <= 1 && Math.abs(j - cursor.j) <= 1)
+    if (Math.abs(i - I) <= 1 && Math.abs(j - J) <= 1)
       return false;
-    if (!(i === cursor.i || j === cursor.j || Math.abs(i - cursor.i) === Math.abs(j - cursor.j)))
+    if (!(i === I || j === J || Math.abs(i - I) === Math.abs(j - J)))
       return false;
-    var di = Math.sign(i - cursor.i)
-      var dj = Math.sign(j - cursor.j);
-    var ii = cursor.i;
-    var jj = cursor.j;
+    var di = Math.sign(i - I);
+    var dj = Math.sign(j - J);
+    var ii = I;
+    var jj = J;
     while (true) {
       ii += di;
       jj += dj;
       if (ii === i && jj === j)
         return true;
-      if (!array[ii][jj].exist)
+      if (state[ii][jj] === blank)
         return false;
     }
   }
 
+  function can_hold(i, j) {
+    return !holding && state[i][j] !== blank;
+  }
+
   function hold(i, j) {
     holding = true;
-    cursor.i = i;
-    cursor.j = j;
+    I = i;
+    J = j;
     update();
   }
 
   function move(i, j) {
-    var di = Math.sign(i - cursor.i);
-    var dj = Math.sign(j - cursor.j);
-    var ii = cursor.i;
-    var jj = cursor.j;
+    var di = Math.sign(i - I);
+    var dj = Math.sign(j - J);
+    var ii = I;
+    var jj = J;
     while (true) {
       ii += di;
       jj += dj;
       if (ii === i && jj === j)
         break;
-      array[ii][jj].omote = !array[ii][jj].omote
+      state[ii][jj] = state[ii][jj] === obverse ? reverse : obverse;
     }
-    array[i][j].exist = true;
-    array[i][j].omote = array[cursor.i][cursor.j].omote;
-    array[cursor.i][cursor.j].exist = false;
-    cursor.i = i;
-    cursor.j = j;
+    state[i][j] = state[I][J];
+    state[I][J] = blank;
+    I = i;
+    J = j;
     update();
   }
 
   function release() {
-    count++;
     holding = false;
+    count++;
+    update();
+  }
+
+  function load() {
+    for (var i = 0; i < size; i++)
+      for (var j = 0; j < size; j++)
+        state[i][j] = saved[i][j];
     update();
   }
 })();
