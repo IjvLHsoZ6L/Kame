d0835b86dda382fb61215faffd834d456a3f85e5
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-03-01 06:20:07 +0900

    modified:   solver.hs

--- solver.hs
+++ solver.hs
@@ -1,99 +1,122 @@
-import Control.Applicative
-import Data.Array
--- import Data.Set
+import Data.Array.Unboxed
+import Data.Set (empty, member, insert)
 
 main :: IO ()
 main = do
-    b <- read <$> readFile "problem.txt" :: IO Board
-    print b
-    print $ move b (1, 0) (3, 0)
-    print $ move (move b (1, 0) (3, 0)) (3, 0) (3, 3)
-    print $ move (move (move b (1, 0) (3, 0)) (3, 0) (3, 3)) (3, 3) (0, 0)
+    s <- readFile "problem.txt"
+    let bStart = toBoard s
+    printBoard bStart
+    mapM_ printBoardProcess [ (b, pr) | (b, pr) <- tranforms bStart , isClear b ]
+    where
 
-data State = Blank | Obverse | Reverse
+type Board = UArray Position State
 
-fromState :: State -> Char
-fromState Blank   = '.'
-fromState Obverse = 'o'
-fromState Reverse = 'x'
+type State = Char
 
-toState :: Char -> State
-toState '.' = Blank
-toState 'o' = Obverse
-toState 'x' = Reverse
-toState _   = error "unknown charcter"
+type Position = (Int, Int)
 
-data Board = Board
-    { height :: Int
-    , width  :: Int
-    , states :: Array (Int, Int) State
-    }
+type Path = [Position]
 
-instance Show Board where
-    show b
-        = unlines
-            [
-                [ fromState (states b ! (i, j))
-                | j <- range (0, width b - 1)
-                ]
-            | i <- range (0, height b - 1)
-            ]
+type Process = [Path]
 
-instance Read Board where
-    readsPrec _ s = [(b, "")] where
-        b = Board
-            { height = h
-            , width  = w
-            , states = array ((0, 0), (h - 1, w - 1)) ies
-            }
-        h = length ss
-        w = length (head ss)
-        ss = lines s
-        ies = [ ((i, j), toState (ss !! i !! j))
-            | (i, j) <- range ((0, 0), (h - 1, w - 1)) ]
+height :: Int
+height = 4
 
-exists :: Board -> (Int, Int) -> Bool
-exists b p = case states b ! p of
-    Blank -> False
-    _     -> True
+width :: Int
+width = 4
 
-canMove :: Board -> (Int, Int) -> (Int, Int) -> Bool
-canMove b (i0, j0) (i1, j1)
-    | exists b (i1, j1)
-        = False
+positionBound :: (Position, Position)
+positionBound = ((0, 0), (height - 1, width - 1))
+
+positions :: [Position]
+positions = range positionBound
+
+toBoard :: String -> Board
+toBoard s = array positionBound [ ((i, j), lines s !! i !! j) | (i, j) <- positions ]
+
+fromBoard :: Board -> String
+fromBoard b = unlines [ [ b ! (i, j) | j <- [0 .. width - 1] ] | i <- [0 .. height - 1] ]
+
+printBoard :: Board -> IO ()
+printBoard = putStrLn . fromBoard
+
+printBoardProcess :: (Board, Process) -> IO ()
+printBoardProcess (b, pr) = do
+    print (reverse (map reverse pr))
+    printBoard b
+
+tranforms :: Board -> [(Board, Process)]
+tranforms bStart = loop empty [(bStart, [])] where
+    loop _ [] = []
+    loop found ((b, pr) : rest)
+        | b `member` found
+            = loop found rest
+        | otherwise
+            = (b, pr)
+            : loop (b `insert` found) (rest
+                ++ [ (b', path : pr) | (b', path) <- tranformsByPath b ])
+
+tranformsByPath :: Board -> [(Board, Path)]
+tranformsByPath bStart = loop empty [(bStart, p, [p]) | p <- positions , exists bStart p ] where
+    loop _ [] = []
+    loop found ((b, p, path) : rest)
+        | (b, p) `member` found
+            = loop found rest
+        | otherwise
+            = (b, path)
+            : loop ((b, p) `insert` found) (rest
+                ++ [ (move b p q, q, q : path) | q <- positions , canMove b p q ])
+
+isClear :: Board -> Bool
+isClear b = and [ b ! p /= 'x' | p <- positions ]
+
+canMove :: Board -> Position -> Position -> Bool
+canMove b p q = isBlank b q
+    && canJoin p q
+    && and [ exists b r | r <- segment p q ]
+
+move :: Board -> Position -> Position -> Board
+move b p q = b
+    // [ (p, '.') ]
+    // [ (r, invert (b ! r)) | r <- segment p q ]
+    // [ (q, b ! p) ]
+
+exists :: Board -> Position -> Bool
+exists b p = b ! p == 'o' || b ! p == 'x'
+
+isBlank :: Board -> Position -> Bool
+isBlank b p = b ! p == '.'
+
+invert :: State -> State
+invert 'o' = 'x'
+invert 'x' = 'o'
+invert _   = error "invert"
+
+canJoin :: Position -> Position -> Bool
+canJoin (i0, j0) (i1, j1)
     | abs (i1 - i0) <= 1 && abs (j1 - j0) <= 1
         = False
     | i0 == i1
-        = and [ exists b (i0, j) | j <- js ]
+        = True
     | j0 == j1
-        = and [ exists b (i, j0) | i <- is ]
-    | i1 - i0 == j1 - j0
-        = and [ exists b p | p <- zip is js ]
-    | i1 - i0 == j0 - j1
-        = and [ exists b p | p <- zip is (reverse js) ]
+        = True
+    | abs (i1 - i0) == abs (j1 - j0)
+        = True
     | otherwise
         = False
-    where
-        is = range (min i0 i1 + 1, max i0 i1 - 1)
-        js = range (min j0 j1 + 1, max j0 j1 - 1)
 
-move :: Board -> (Int, Int) -> (Int, Int) -> Board
-move b (i0, j0) (i1, j1) = b { states = states b // updates } where
-    updates = ((i0, j0), Blank) : ((i1, j1), states b ! (i0, j0))
-            : [ (p, turn $ states b ! p) | p <- segment ]
-    segment
-        | i0 == i1
-            = zip (repeat i0) js
-        | j0 == j1
-            = zip is (repeat j0)
-        | i1 - i0 == j1 - j0
-            = zip is js
-        | i1 - i0 == j0 - j1
-            = zip is (reverse js)
-        | otherwise
-            = error "error"
-    is = range (min i0 i1 + 1, max i0 i1 - 1)
-    js = range (min j0 j1 + 1, max j0 j1 - 1)
-    turn Obverse = Reverse
-    turn Reverse = Obverse
-    turn _       = error "error"
+segment :: Position -> Position -> [Position]
+segment (i0, j0) (i1, j1)
+    | i0 == i1
+        = [ (i0, j) | j <- js ]
+    | j0 == j1
+        = [ (i, j0) | i <- is ]
+    | i1 - i0 == j1 - j0
+        = zip is js
+    | i1 - i0 == j0 - j1
+        = zip is (reverse js)
+    | otherwise
+        = error "segment"
+    where
+        is = [min i0 i1 + 1 .. max i0 i1 - 1]
+        js = [min j0 j1 + 1 .. max j0 j1 - 1]
