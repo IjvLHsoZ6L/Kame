5021a694a32b6eb936abb4751fec8d2ceb2f59bd
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-03-01 21:08:44 +0900

    modified:   solver.hs

--- solver.hs
+++ solver.hs
@@ -1,13 +1,15 @@
-import Data.Array.Unboxed
-import Data.Set (empty, member, insert)
+import Control.Applicative ((<$>))
+import Data.Array.Unboxed (UArray, array, range, (!), (//))
+import Data.Set (Set, empty, singleton, notMember, insert)
+import Data.Sequence (Seq, ViewL(EmptyL, (:<)), viewl, fromList, (|>), (><))
+import qualified Data.Sequence as Q (empty, singleton)
 
 main :: IO ()
 main = do
-    s <- readFile "problem.txt"
-    let bStart = toBoard s
-    printBoard bStart
-    mapM_ printBoardProcess [ (b, pr) | (b, pr) <- tranforms bStart , isClear b ]
-    where
+    b0 <- toBoard <$> readFile "problem.txt"
+    printBoard b0
+    printBoardProcess $ head $ filter (isClear . fst) $ transform b0
+    -- mapM_ printBoardProcess $ transform b0
 
 type Board = UArray Position State
 
@@ -42,44 +44,37 @@ printBoard = putStrLn . fromBoard
 
 printBoardProcess :: (Board, Process) -> IO ()
 printBoardProcess (b, pr) = do
-    print (reverse (map reverse pr))
+    mapM_ print (reverse (map reverse pr))
     printBoard b
-
-tranforms :: Board -> [(Board, Process)]
-tranforms bStart = loop empty [(bStart, [])] where
-    loop _ [] = []
-    loop found ((b, pr) : rest)
-        | b `member` found
-            = loop found rest
-        | otherwise
-            = (b, pr)
-            : loop (b `insert` found) (rest
-                ++ [ (b', path : pr) | (b', path) <- tranformsByPath b ])
-
-tranformsByPath :: Board -> [(Board, Path)]
-tranformsByPath bStart = loop empty [(bStart, p, [p]) | p <- positions , exists bStart p ] where
-    loop _ [] = []
-    loop found ((b, p, path) : rest)
-        | (b, p) `member` found
-            = loop found rest
-        | otherwise
-            = (b, path)
-            : loop ((b, p) `insert` found) (rest
-                ++ [ (move b p q, q, q : path) | q <- positions , canMove b p q ])
 
 isClear :: Board -> Bool
 isClear b = and [ b ! p /= 'x' | p <- positions ]
 
+transform :: Board -> [(Board, Process)]
+transform b0 = loop Q.empty (Q.singleton (b0, [])) empty (singleton b0) where
+    loop :: Seq (Board, Position, Path, Process) -> Seq (Board, Process)
+            -> Set (Board, Position) -> Set Board -> [(Board, Process)]
+    loop bppps bprs bpset bset = case viewl bppps of
+        EmptyL -> case viewl bprs of
+            EmptyL
+                -> []
+            (b, pr) :< bprs'
+                -> loop (bppps >< fromList [ (b, p, [], pr) | p <- positions , exists b p ]) bprs' bpset bset
+        (b, p, ps, pr) :< bppps'
+            | b `notMember` bset
+                -> (b, (p : ps) : pr)
+                : loop bppps (bprs |> (b, (p : ps) : pr)) bpset (b `insert` bset)
+            | (b, p) `notMember` bpset
+                -> loop (bppps' >< fromList [ (move b p q, q, p : ps, pr) | q <- positions , canMove b p q ])
+                    bprs ((b, p) `insert` bpset) bset
+            | otherwise
+                -> loop bppps' bprs bpset bset
+
 canMove :: Board -> Position -> Position -> Bool
-canMove b p q = isBlank b q
-    && canJoin p q
-    && and [ exists b r | r <- segment p q ]
+canMove b p q = isBlank b q && canJoin p q && and [ exists b r | r <- segment p q ]
 
 move :: Board -> Position -> Position -> Board
-move b p q = b
-    // [ (p, '.') ]
-    // [ (r, invert (b ! r)) | r <- segment p q ]
-    // [ (q, b ! p) ]
+move b p q = b // ((p, '.') : (q, b ! p) : [ (r, invert (b ! r)) | r <- segment p q ])
 
 exists :: Board -> Position -> Bool
 exists b p = b ! p == 'o' || b ! p == 'x'
@@ -96,14 +91,8 @@ canJoin :: Position -> Position -> Bool
 canJoin (i0, j0) (i1, j1)
     | abs (i1 - i0) <= 1 && abs (j1 - j0) <= 1
         = False
-    | i0 == i1
-        = True
-    | j0 == j1
-        = True
-    | abs (i1 - i0) == abs (j1 - j0)
-        = True
     | otherwise
-        = False
+        = i0 == i1 || j0 == j1 || abs (i1 - i0) == abs (j1 - j0)
 
 segment :: Position -> Position -> [Position]
 segment (i0, j0) (i1, j1)
