17ce5b19dc6abbef5b3f85ca5b89e6981e9d009b
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-03-10 22:06:05 +0900

    modified:   index.html script.js

--- script.js
+++ script.js
@@ -15,7 +15,12 @@
   var config;
   var count, holding, I, J;
   var state, table, image;
-  var saved = [[0, 0, 0, 0], [2, 2, 2, 2], [2, 2, 2, 2], [0, 2, 2, 0]];
+  var saved =
+  [ [[0, 0, 0, 0], [2, 2, 2, 2], [2, 2, 2, 2], [0, 2, 2, 0]]
+  , [[0, 0, 0, 0], [2, 2, 2, 2], [2, 2, 2, 2], [0, 2, 2, 0]]
+  , [[0, 0, 2, 0], [0, 2, 2, 2], [2, 2, 2, 0], [0, 2, 0, 0]]
+  , [[0, 2, 2, 0], [2, 2, 0, 2], [2, 0, 2, 2], [0, 2, 2, 0]]
+  ];
 
   (function initialize() {
 
@@ -49,9 +54,10 @@
 
     document.getElementById('start').onclick = function() {
       if (config) {
+        document.getElementById('select').selectedIndex = 0;
         for (var i = 0; i < size; i++)
           for (var j = 0; j < size; j++)
-            saved[i][j] = state[i][j];
+            saved[0][i][j] = state[i][j];
       }
       config = false;
       load();
@@ -61,121 +67,127 @@
       config = true;
       load();
     };
+
+    document.getElementById('select').onchange = function () {
+      config = false;
+      load();
+    }
   })();
 
-  function load() {
-    count = 0;
-    holding = false;
-    for (var i = 0; i < size; i++)
-      for (var j = 0; j < size; j++)
-        state[i][j] = saved[i][j];
-    update();
-  }
+function load() {
+  count = 0;
+  holding = false;
+  var k = document.getElementById('select').selectedIndex;
+  for (var i = 0; i < size; i++)
+    for (var j = 0; j < size; j++)
+      state[i][j] = saved[k][i][j];
+  update();
+}
 
-  function update() {
-    document.getElementById('count').value = count;
-    var isCompleted = true;
-    for (var i = 0; i < size; i++) {
-      for (var j = 0; j < size; j++) {
-        if (state[i][j] === blank)
-          image[i][j].style.visibility = 'hidden';
-        else {
-          image[i][j].style.visibility = 'visible';
-          image[i][j].src = state[i][j] === obverse ? 'obverse.png' : 'reverse.png';
-        }
-        if (config)
-          table[i][j].style.background = gray;
-        else if (selected(i, j))
-          table[i][j].style.background = red;
-        else if (can_move(i, j))
-          table[i][j].style.background = blue;
-        else
-          table[i][j].style.background = green;
-        if (state[i][j] === reverse)
-            isCompleted = false;
-      }
-    }
-    if (!config && !holding && isCompleted)
-      setTimeout(function() { alert('Clear! [count: ' + count + ']'); }, 100);
-  }
-
-  function onclick(i, j) {
-    return function() {
-      if (config) {
-        state[i][j] = (state[i][j] + 1) % 3;
-        update();
-      }
+function update() {
+  document.getElementById('count').value = count;
+  var isCompleted = true;
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      if (state[i][j] === blank)
+        image[i][j].style.visibility = 'hidden';
       else {
-        if (selected(i, j))
-          release();
-        else if (can_move(i, j))
-          move(i, j);
-        else if (can_hold(i, j))
-          hold(i, j);
+        image[i][j].style.visibility = 'visible';
+        image[i][j].src = state[i][j] === obverse ? 'obverse.png' : 'reverse.png';
       }
-    };
-  }
-
-  function selected(i, j) {
-    return holding && i === I && j === J;
-  }
-
-  function can_move(i, j) {
-    if (!holding)
-      return false;
-    if (state[i][j] !== blank)
-      return false;
-    if (Math.abs(i - I) <= 1 && Math.abs(j - J) <= 1)
-      return false;
-    if (!(i === I || j === J || Math.abs(i - I) === Math.abs(j - J)))
-      return false;
-    var di = i > I ? +1 : i < I ? -1 : 0;
-    var dj = j > J ? +1 : j < J ? -1 : 0;
-    var ii = I;
-    var jj = J;
-    while (true) {
-      ii += di;
-      jj += dj;
-      if (ii === i && jj === j)
-        return true;
-      if (state[ii][jj] === blank)
-        return false;
+      if (config)
+        table[i][j].style.background = gray;
+      else if (selected(i, j))
+        table[i][j].style.background = red;
+      else if (can_move(i, j))
+        table[i][j].style.background = blue;
+      else
+        table[i][j].style.background = green;
+      if (state[i][j] === reverse)
+        isCompleted = false;
     }
   }
+  if (!config && !holding && isCompleted)
+    setTimeout(function() { alert('Clear! [count: ' + count + ']'); }, 100);
+}
 
-  function can_hold(i, j) {
-    return !holding && state[i][j] !== blank;
-  }
-
-  function hold(i, j) {
-    holding = true;
-    I = i;
-    J = j;
-    update();
-  }
-
-  function move(i, j) {
-    var di = i > I ? +1 : i < I ? -1 : 0;
-    var dj = j > J ? +1 : j < J ? -1 : 0;
-    var ii = I;
-    var jj = J;
-    while (true) {
-      ii += di;
-      jj += dj;
-      if (ii === i && jj === j)
-        break;
-      state[ii][jj] = state[ii][jj] === obverse ? reverse : obverse;
+function onclick(i, j) {
+  return function() {
+    if (config) {
+      state[i][j] = (state[i][j] + 1) % 3;
+      update();
     }
-    state[i][j] = state[I][J];
-    state[I][J] = blank;
-    I = i;
-    J = j;
-    update();
-  }
+    else {
+      if (selected(i, j))
+        release();
+      else if (can_move(i, j))
+        move(i, j);
+      else if (can_hold(i, j))
+        hold(i, j);
+    }
+  };
+}
 
-  function release() {
-    holding = false;
-    count++;
-    update();
+function selected(i, j) {
+  return holding && i === I && j === J;
+}
+
+function can_move(i, j) {
+  if (!holding)
+    return false;
+  if (state[i][j] !== blank)
+    return false;
+  if (Math.abs(i - I) <= 1 && Math.abs(j - J) <= 1)
+    return false;
+  if (!(i === I || j === J || Math.abs(i - I) === Math.abs(j - J)))
+    return false;
+  var di = i > I ? +1 : i < I ? -1 : 0;
+  var dj = j > J ? +1 : j < J ? -1 : 0;
+  var ii = I;
+  var jj = J;
+  while (true) {
+    ii += di;
+    jj += dj;
+    if (ii === i && jj === j)
+      return true;
+    if (state[ii][jj] === blank)
+      return false;
   }
+}
+
+function can_hold(i, j) {
+  return !holding && state[i][j] !== blank;
+}
+
+function hold(i, j) {
+  holding = true;
+  I = i;
+  J = j;
+  update();
+}
+
+function move(i, j) {
+  var di = i > I ? +1 : i < I ? -1 : 0;
+  var dj = j > J ? +1 : j < J ? -1 : 0;
+  var ii = I;
+  var jj = J;
+  while (true) {
+    ii += di;
+    jj += dj;
+    if (ii === i && jj === j)
+      break;
+    state[ii][jj] = state[ii][jj] === obverse ? reverse : obverse;
+  }
+  state[i][j] = state[I][J];
+  state[I][J] = blank;
+  I = i;
+  J = j;
+  update();
+}
+
+function release() {
+  holding = false;
+  count++;
+  update();
+}
 })();
